e0dd8e0c63b447484352838ccb0a341126a35f54
mszell <markus.zellner@gfk.com>
2021-05-10 10:27:30 +0200

    Add sceleton

---
+++ src/main/java/com/gfk/tdd/shuffler/ShufflerApplication.java
@@ -0,0 +1,13 @@
+package com.gfk.tdd.shuffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShufflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShufflerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gfk/tdd/shuffler/ShufflerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfk.tdd.shuffler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShufflerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
